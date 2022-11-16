c3649351cd4769b11aaba6f5ce34c361d75d6522
Josh Hawks <snl95@txstate.edu>
2022-11-15 18:45:39 -0600

    navbar responsive/ more styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,37 +1,68 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleNavbar = () => {
+		setIsOpen(!isOpen);
+	};
+
+	console.log(isOpen);
+
 	return (
-		// use tailwindcss to style the navbar
-		<nav
-			className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono'
-			role='navigation'
-		>
-			<Link to='/' className='pl-8'>
-				Escapade
-			</Link>
-			<div className='px-4 cursor-pointer md:hidden'>
-				<svg
-					className='w-6 h-6'
-					fill='none'
-					stroke='currentColor'
-					viewBox='0 0 24 24'
-					xmlns='http://www.w3.org/2000/svg'
+		<nav className=' bg-indigo-800 py-4 px-11 w-screen md:flex md:justify-between md:items-center'>
+			<div className='flex items-center justify-between'>
+				<Link
+					to='/'
+					className='text-xl font-bold text-white md:text-2xl'
+					href='/'
 				>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						strokeWidth='2'
-						d='M4 6h16M4 12h16M4 18h16'
-					></path>
-				</svg>
+					Escapade
+				</Link>
+
+				<div
+					onClick={toggleNavbar}
+					className='flex md:hidden'
+					onKeyup={toggleNavbar}
+				>
+					<button
+						type='button'
+						className='text-white hover:text-gray-400 focus:outline-none focus:text-gray-400'
+					>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke-width='1.5'
+							stroke='currentColor'
+							className='w-6 h-6'
+						>
+							<path
+								stroke-linecap='round'
+								stroke-linejoin='round'
+								d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+							/>
+						</svg>
+					</button>
+				</div>
 			</div>
-			<div className='pr-8 md:block hidden'>
-				<Link to='/adventures' className='p-4'>
+
+			<div
+				className={`flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 items-end ${
+					isOpen ? 'flex' : 'hidden'
+				}`}
+			>
+				<Link
+					to='/adventures'
+					className=' text-center w-28 text-white text-lg border border-solid border-white rounded-md p-2 hover:text-gray-800 hover:bg-slate-100'
+				>
 					Adventures
 				</Link>
-				<Link to='/create' className='p-4'>
+				<Link
+					to='/create'
+					className='text-center w-28 text-white text-lg border border-solid border-white rounded-md p-2 hover:text-gray-800 hover:bg-slate-100'
+				>
 					Create
 				</Link>
 			</div>
